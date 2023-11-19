38cca086897376fc6e2face8d8cd7d733bdd2cd6
NizamPMNA <nizamvtpmna@gmail.com>
2023-11-19 21:39:20 +0530

    updation in user registration

--- login/views.py
+++ login/views.py
@@ -1,15 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
 from .models import Register
+from django.contrib.auth import authenticate,login
 # Create your views here.
-def login(request):
-    
-    return render(request,'login.html')
 
 def register(request):
+    user = None
+    error_msg = None
     if request.POST:
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        reg_obj = Register(email=email,password=password)
-        reg_obj.save()
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        try:
+            user = User.objects.create_user(username=username,password=password,email=email)
+        except Exception:
+            error_msg = username
+        if user:
+            return redirect('login')
+            
     
-    return render(request,'register.html')
+    return render(request,'register.html',{'user':user,'error_msg':error_msg})
+
+def login(request):
+    error_msg = None
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username,password=password)
+        if user:
+            login(request,user)
+            return redirect('index')
+        else:
+            error_msg = 'invalid credencials'
+
+    return render(request,'login.html',{'error_msg':error_msg})
